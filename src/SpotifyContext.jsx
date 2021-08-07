13bb3ef4bc2345599820a38ac51804783ee4dff1
Greg <gregybos@gmail.com>
2021-08-07 10:42:04 -0500

    adding acios to SpotifyContext

--- src/SpotifyContext.jsx
+++ src/SpotifyContext.jsx
@@ -1,18 +1,29 @@
 import React, { createContext, useState } from 'react';
+import axios from 'axios';
 
 const SpotifyContext = createContext();
 
 const SpotifyContextProvider = ({children}) => {
   const [ spotifyUser, setSpotifyUser ] = useState({});
   const { googleUser, setGoogleUser } = useState({});
+  const { token, setToken } = useState('');
+  const { tokenSecret, setTokenSecret } = useState('');
 
-  const 
+  const authSpotify = () => {
+    axios.get()
+
+  };
 
   const SpotifyProps = {
     spotifyUser,
     setSpotifyUser,
     googleUser,
-    setGoogleUser
+    setGoogleUser,
+    token,
+    setToken,
+    tokenSecret,
+    setTokenSecret,
+    authSpotify
   };
 
   return (

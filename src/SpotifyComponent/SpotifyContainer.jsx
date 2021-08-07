bad334412b685be8895b0ab4cfecbb42e2c66834
Greg <gregybos@gmail.com>
2021-08-07 14:10:36 -0500

    adding spotifyContainer to hold results

---
+++ src/SpotifyComponent/SpotifyContainer.jsx
@@ -0,0 +1,10 @@
+import React, { useContext }from 'react';
+
+import SpotifyContext from '../SpotifyContext.jsx';
+
+const SpotifyContainer = () => {
+  const { spotifyUser } = useContext(SpotifyContext);
+  
+};
+
+export default SpotifyContainer;
